5122888321d99ccb6c5399c3a09b832e206ea722
Prince Kumar <princekumar036@gmail.com>
2021-04-22 01:43:02 +0530

    inserted my credentials

--- script.js
+++ script.js
@@ -21,10 +21,10 @@ $( document ).ready(function() {
 
     // AUTHORIZE with Spotify (if needed)
     // *************** REPLACE THESE VALUES! *************************
-    let client_id = 'df6968c34f4b4494ac206c7628d2afc7';
+    let client_id = 'e0cbeeefd8fe4943acb72842b2a047ee';
     // Use the following site to convert your regular url to the encoded version:
     // https://www.url-encode-decode.com/
-    let redirect_uri = 'https%3A%2F%2Fmujibsardar.github.io%2Fspotify_jquery_only'; // GitHub Pages URL or whatever your public url to this app is
+    let redirect_uri = 'https%3A%2F%2Fprincekumar036.github.io%2Flinguistify%2F'; // GitHub Pages URL or whatever your public url to this app is
     // *************** END *************************
 
     const redirect = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}`;
